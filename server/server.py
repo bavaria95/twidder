987001b45eb47f3b4bdc53e35a4d425c063424b4
Dima Petruk <bavaria95@gmail.com>
2016-02-24 15:44:59 +0100

    CORS module

--- server/server.py
+++ server/server.py
@@ -1,8 +1,10 @@
 from flask import Flask, request
 import json
 import database_helper
+from flask.ext.cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.before_request
 def before_request():
