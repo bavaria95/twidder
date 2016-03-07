3eea46aa3d220008906db82117d9c44470f191f0
Dima Petruk <bavaria95@gmail.com>
2016-03-07 21:40:24 +0100

    testing login with wrong credentials

--- twidder/testing/test.py
+++ twidder/testing/test.py
@@ -12,6 +12,19 @@ class PythonOrgSearch(unittest.TestCase):
         driver.get("http://127.0.0.1:5000")
         self.assertIn("Twidder", driver.title)
 
+    def test_login(self):
+        driver = self.driver
+        driver.get("http://127.0.0.1:5000")
+        login_mail = driver.find_element_by_name("email")
+        login_mail.send_keys("nonexistedmail@gmail.com")
+
+        login_pass = driver.find_element_by_name("pass")
+        login_pass.send_keys("wrong pass")
+
+        driver.find_element_by_id('login-button').click()
+        
+        self.assertIn('Wrong', driver.find_element_by_id('error-log').text)
+    
     def tearDown(self):
         self.driver.close()
 
