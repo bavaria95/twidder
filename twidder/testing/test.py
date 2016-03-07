9bb253f98c69f10a5996b7a143e5df4ca6c51dc2
Dima Petruk <bavaria95@gmail.com>
2016-03-07 21:45:08 +0100

    checking whether user can login and see his info

--- twidder/testing/test.py
+++ twidder/testing/test.py
@@ -25,6 +25,23 @@ class PythonOrgSearch(unittest.TestCase):
         
         self.assertIn('Wrong', driver.find_element_by_id('error-log').text)
     
+    def test_user_info(self):
+        driver = self.driver
+        driver.get("http://127.0.0.1:5000")
+        login_mail = driver.find_element_by_name("email")
+        login_mail.send_keys("bavaria95@gmail.com")
+
+        login_pass = driver.find_element_by_name("pass")
+        login_pass.send_keys("12345678")
+
+        driver.find_element_by_id('login-button').click()
+        
+        driver.find_element_by_id('home-tab').click()
+
+        print(driver.find_element_by_id('home-email').text)
+        self.assertIn('bavaria95@gmail.com', driver.find_element_by_id('home-email').text)
+
+
     def tearDown(self):
         self.driver.close()
 
