4b89bf8ee44c54ea07eec099d83be8ab2da5b6b0
Dima Petruk <bavaria95@gmail.com>
2016-03-07 21:49:59 +0100

    small test refactoring

--- twidder/testing/test.py
+++ twidder/testing/test.py
@@ -15,35 +15,34 @@ class PythonOrgSearch(unittest.TestCase):
     def test_login(self):
         driver = self.driver
         driver.get("http://127.0.0.1:5000")
-        login_mail = driver.find_element_by_name("email")
-        login_mail.send_keys("nonexistedmail@gmail.com")
 
-        login_pass = driver.find_element_by_name("pass")
-        login_pass.send_keys("wrong pass")
-
-        driver.find_element_by_id('login-button').click()
-        
+        login(driver, "nonexistedmail@gmail.com", "wrong pass")
+                
         self.assertIn('Wrong', driver.find_element_by_id('error-log').text)
     
     def test_user_info(self):
         driver = self.driver
         driver.get("http://127.0.0.1:5000")
-        login_mail = driver.find_element_by_name("email")
-        login_mail.send_keys("bavaria95@gmail.com")
 
-        login_pass = driver.find_element_by_name("pass")
-        login_pass.send_keys("12345678")
-
-        driver.find_element_by_id('login-button').click()
+        login(driver, 'bavaria95@gmail.com', '12345678')
         
         driver.find_element_by_id('home-tab').click()
 
-        print(driver.find_element_by_id('home-email').text)
         self.assertIn('bavaria95@gmail.com', driver.find_element_by_id('home-email').text)
 
 
     def tearDown(self):
         self.driver.close()
+
+def login(driver, email, passwd):
+    login_mail = driver.find_element_by_name("email")
+    login_mail.send_keys(email)
+
+    login_pass = driver.find_element_by_name("pass")
+    login_pass.send_keys(passwd)
+
+    driver.find_element_by_id('login-button').click()
+
 
 if __name__ == "__main__":
     unittest.main()
