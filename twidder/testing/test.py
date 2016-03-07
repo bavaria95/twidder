18743cc4388f4126167e7bce58151c7c37fc2eef
Dima Petruk <bavaria95@gmail.com>
2016-03-07 22:26:52 +0100

    testing posting on the wall

--- twidder/testing/test.py
+++ twidder/testing/test.py
@@ -2,6 +2,7 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+
 class PythonOrgSearch(unittest.TestCase):
 
     def setUp(self):
@@ -44,6 +45,25 @@ class PythonOrgSearch(unittest.TestCase):
         driver.find_element_by_id('home-tab').click()
 
         self.assertIn('bavaria95@gmail.com', driver.find_element_by_id('home-email').text)
+
+    def test_posting(self):
+        '''
+        Logs in, navigates to Home tab, enters message in the input field, 
+        click Post button and the see whether new message has appeared
+        '''
+
+        driver = self.driver
+        driver.get("http://127.0.0.1:5000")
+
+        login(driver, 'bavaria95@gmail.com', '12345678')
+        
+        driver.find_element_by_id('home-tab').click()
+
+        driver.find_element_by_id('status-field-home').send_keys('Uniq message!')
+        driver.find_element_by_id('status-send-home').click()
+        driver.refresh()
+
+        self.assertIn('Uniq message!', driver.find_element_by_tag_name('li').text)
 
 
     def tearDown(self):
